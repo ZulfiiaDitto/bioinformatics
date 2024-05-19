df0cab67e530befdf6e3f39c5c0eff122a629020
ZulfiiaDitto <zulfiia.ditto@gmail.com>
2024-05-18 20:41:02 -0500

    adding comment and checking git

--- DNA_toolkit/main.py
+++ DNA_toolkit/main.py
@@ -1,6 +1,6 @@
 from dna_toolkit import *
 import random
-# testing enviroment 
+# testing enviroment to make sure fucntions are working  
 
 randDNA = ''.join([random.choice(nucleotides) for nuc in range(20)])
 dna = validateSeq(randDNA)
