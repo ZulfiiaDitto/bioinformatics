ce1746bcf6972c72b571e12df35de8c29611aa13
ZulfiiaDitto <zulfiia.ditto@gmail.com>
2024-06-17 20:04:12 -0500

    Preeclapsia progress

--- Analysis/Macroarray analysis/Preeclampsia microarray analysis.R
+++ Analysis/Macroarray analysis/Preeclampsia microarray analysis.R
@@ -1,3 +1,4 @@
+# setwd("~/Documents/bioinformatics/Analysis/Macroarray analysis")
 # analysis used publically available data from EMBL-EBI
 # R analysis happend on Jun 11 2024
 
@@ -6,6 +7,9 @@
 # Includes 47,000 genes screening, two chanel microarray 
 # expression of the genes from 
 # placenta of 13 normal and 14 pathological pregnancy, 
+
+
+library(preprocessCore)
 
 # 1. read the table with ref data 
 # downloaded files have isue and need to be pre-processed: 
@@ -18,7 +22,6 @@ df <- read.table('Reference.txt', sep="\t", header = TRUE)
 
 url <- df$Comment..Derived.ArrayExpress.FTP.file.[1]
 
-print(url)
 # 2. get data from the ftp.file
 
 temp <- tempfile()
@@ -50,12 +53,64 @@ colnames(final) <- gsub(pattern, "greenMeanSignal", colnames(final))
 pattern2 <- "GEO:AGILENT_rMeanSignal"
 colnames(final) <- gsub(pattern2, "redMeanSignal", colnames(final))
 
+# after reviwing the table -> certain rows need to be drop since they are not the probes
+
+final <- final[1:41675,]
+
 # 4 normalization 
 
+oligo <- final$`Scan.REF_Reporter REF`
 
+#4a converting everything to numeric 
+for (i in 2:ncol(final)) {
+  final[, i] <- as.numeric(final[, i])
+}
+#4b converting to matcix
+matrix <- as.matrix(final[,-1])
+#4c finally normalization 
+normalized_data <- normalize.quantiles(matrix)
 
+# Combine normalized data with gene identifiers
 
+normalized_final <- cbind(Gene = oligo, as.data.frame(normalized_data))
+colnames(normalized_final)[-1] <- colnames(final)[-1]
 
+# Check the structure of the normalized data
+str(normalized_final)
 
+boxplot(as.data.frame(normalized_data), main = "Boxplot of Normalized Data", las = 2)
 
+# Log2 transformation
+log2_normalized_data <- log2(normalized_data + 1)
 
+# box polot looks little bit better 
+boxplot(as.data.frame(log2_normalized_data), main = "Boxplot of Normalized Data", las = 2)
+
+# T test 
+control_samples = normalized_final[, c(6,7, 10,11, 14, 15,18,19 )]
+sample = normalized_final[, -c(6,7, 10,11, 14, 15,18,19 )]
+
+p_values <- apply(normalized_data, 1, function(row) {
+  control_samples <- row[c(6,7, 10,11, 14, 15,18,19)]
+  sample <- row[-c(6,7, 10,11, 14, 15,18,19 )]
+  t.test(control_samples, sample)$p.value
+})
+
+results <- data.frame(Gene = oligo, p_value = p_values)
+
+# Filter significant genes
+significant_genes <- results[results$p_value < 0.05, ]
+
+# next step is to pull what each oligo is represent, gene location 
+library(biomaRt)
+
+ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl") 
+
+annotated_ids <- c(significant_genes$Gene) 
+# TODO: figurate out the biomart API 
+genes <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"),
+               filters = "", values = annotated_ids,
+               mart = ensembl)
+
+# Display the retrieved gene annotations
+print(listFilters(ensembl))
