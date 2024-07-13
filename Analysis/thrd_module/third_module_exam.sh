57681f976a7e22349b0b8319a397dbc4b4844d3c
ZulfiiaDitto <zulfiia.ditto@gmail.com>
2024-07-12 21:32:52 -0500

    exam answers - not all

--- Analysis/thrd_module/third_module_exam.sh
+++ Analysis/thrd_module/third_module_exam.sh
@@ -25,6 +25,61 @@ more wu_0.v7.fas|  grep ">" | tail -n 1
 grep -c "/1" wu_0_A_wgs.fastq
 # answer 147487
 
+# question7 7. How many matches (alignments) were reported for the original (full-match) setting? Exclude lines in the file containing unmapped reads.
+bowtie2 -p 4 -x index/wu_0_idx -U wu_0_A_wgs.fastq -S out.full.sam
+# answer: 147354 (all reads) - 9636(aligned zero time) = 137718 (137719)
+
+# question 8 
+#How many matches (alignments) were reported with the local-match setting? Exclude lines in the file containing unmapped reads.
+bowtie2 -p 4 -x index/wu_0_idx -U wu_0_A_wgs.fastq -S out.full.sam
+# answer: 147354 - 6823 = 140531
+
+# question9 How many reads were mapped in the scenario in Question 7?
+# 93780 + 43938 = 137718
+
+# quesiton 10 How many reads were mapped in the scenario in Question 8?
+# 88935 + 51596 = 140531
+
+# question 11
+# How many reads had multiple matches in the scenario in Question 7?
+# answer 43938 (29.82%) aligned >1 times
+
+# question 12 
+#How many reads had multiple matches in the scenario in Question 8? Use the format above. You can တ†nd this in the bowtie2 summary; note that by default bowtie2 only reports the best match for each read.
+# answer 51596 (35.01%) aligned >1 times
+
+# question 13 
+#How many alignments contained insertions and/or deletions, in the scenario in Question 7?
+cat out.full.sam | grep -v "^#" | cut -f6 | grep -c 'D' 
+#1395
+cat out.full.sam | grep -v '^#' | cut -f6 | grep -c  'I'    
+#1429
+cat out.full.sam | grep -v '^#' | cut -f6 | grep 'D' | grep 'I' | wc -l
+#42
+# answer: 1395 + 1429 + 42 = 2866
+
+# question 14 14. How many alignments contained insertions and/or deletions, in the scenario in Question 8?
+cat out.local.sam | grep -v '^#' | cut -f6 |grep -c 'D' 
+#1453
+cat out.local.sam | grep -v '^#' | cut -f6 |grep -c 'I' 
+#1197
+cat out.local.sam | grep -v '^#' | cut -f6 |grep 'D' | grep 'I' | wc -l 
+#81
+# answer : 1453 + 1197 + 81 = 2731
+
+
+# question 15 How many entries were reported for Chr3?
+
+
+
+
+
+
+
+
+
+
+
 
 
 
